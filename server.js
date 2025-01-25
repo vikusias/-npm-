70b16d334c4217c35351e21d3f539f49ab0c6b4e
Victoria Shumskaya <shumvikashum@gmail.com>
2025-01-26 00:06:28 +0300

    Добавление новых файлов: index.html, server.js и т.д.

---
+++ server.js
@@ -0,0 +1,13 @@
+const math = require('./math');
+
+
+const a = 10;
+const b = 5;
+
+
+const sum = math.add(a, b);
+const difference = math.subtract(a, b);
+
+
+console.log(`Сумма ${a} и ${b} равна: ${sum}`);
+console.log(`Разность ${a} и ${b} равна: ${difference}`);
